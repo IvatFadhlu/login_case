2db337b8df394106ff0757015f468df7be1b4e2f
IvatFadhlu <ivatfadhlu@gmail.com>
2023-05-23 17:05:19 +0700

    first commit

--- server.js
+++ server.js
@@ -8,15 +8,18 @@ app.post("/login", async function (req, res) {
   //ambil data di body
   const data = req.body;
   //ambil data user di database
-  const userData = await postgresqlDb.query('SELECT * FROM user WHERE username = $1', [data.username])
+  const userData = await postgresqlDb.query(
+    "SELECT * FROM user WHERE username = $1",
+    [data.username]
+  );
   //bandingkan username dan password
-  if(userData === null) {
-    res.json({data: 'Email/Password Salah!'})
+  if (userData === null) {
+    res.json({ data: "Email/Password Salah!" });
   } else {
     if (data.password === userData.password) {
-      res.json({data: 'Sukses'})
+      res.json({ data: "Sukses" });
     } else {
-      res.json({data: 'Email/Password Salah!'})
+      res.json({ data: "Email/Password Salah!" });
     }
   }
 });
